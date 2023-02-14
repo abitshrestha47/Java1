69be708d5c968d8d85da93933111776fcf7c31b2
abitshrestha <abitshrestha2079@gmail.com>
2023-02-14 07:23:13 +0545

    javafirstpush

---
+++ Abit.java
@@ -0,0 +1,17 @@
+public class Abit{
+    public static void main(String[] args){
+        System.out.print("Name of the organization:");
+        System.out.println(" Kamalpokhari Restaurant & Bar");
+        System.out.println("\n");
+        System.out.println("Address:");
+        System.out.println("----State: KM");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println("\n");
+        System.out.println("Contact Information:");
+        System.out.print("----Mobile Number: ");
+        System.out.println("9865382409");
+        System.out.print("----Email: ");
+        System.out.println("kamalpokhari.rnb@gmail.com"); 
+    }
+}
