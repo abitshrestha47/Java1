8c45380cfed2bce6781e480f4d3ce46778a0de9f
abitshrestha <abitshrestha2079@gmail.com>
2023-02-16 09:45:45 +0545

    fsd

--- assignment_2/JXOO.java
+++ assignment_2/JXOO.java
@@ -18,8 +18,18 @@ public class JXOO{
         System.out.print("\nTimes the Nuclear waste is removed from the core: ");
         int wasteRemove=takeInput.nextInt();
 
-        System.out.print("\nTotal weight of waste produced in every 1 week: ");
-        float wasteInWeek=takeInput.nextFloat();
+        System.out.print("\nTotal weight of waste produced in every 1 week?Press k or lb");
+        char weightingIn=takeInput.nextLine();
+        double wasteRemoveKg;
+        double wasteRemoveLb;
+        if(weightingIn=="k"){
+             wasteRemoveKg=takeInput.nextDouble();
+             wasteRemoveLb=wasteRemoveKg * 2.20462;
+        }
+        else{
+             wasteRemoveLb=takeInput.nextDouble(); 
+             wasteRemoveKg=wasteRemoveLb/2.20462;
+        }
 
         System.out.println("\nElectric Motor being replaced every 18 days?Press 'y' or 'n' for yes/no respectively");
         char y_n=takeInput.next().charAt(0);
@@ -38,10 +48,12 @@ public class JXOO{
 
         String kelvin2=String.format("%.2f",kelvin);
 
+        System.out.println("Survey information shower");
+
         System.out.println("\nTotal employees present: "+totalEmp);
         System.out.println("Supervisor: "+supervisor);
         System.out.println("Waste Removal in: "+wasteRemove);
-        System.out.println("Waste Produced every 1 week: "+wasteInWeek);
+        System.out.println("Waste Produced every 1 week: "+wasteRemoveKg+"kg/"+wasteRemoveLb+"lbs");
         System.out.println("Electric Motor is being replaced or not: "+yes_no);
         System.out.println("The average temperature of the Nuclear Reactor(CORE): "+temp+"\u00B0C"+"/"+kelvin2+"K");
     }
